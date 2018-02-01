86768b53f20250a35fee7d9b58a7706578e06139
Sergio Jr <sergioamjr91@gmail.com>
2018-01-31 23:58:08 -0200

    Update env

--- src/API_CONFIG.js
+++ src/API_CONFIG.js
@@ -1,6 +1,6 @@
 const API_CONFIG = {
-  CLOSE_API: 'http://localhost:3003/api',
-  AUTH_API: 'http://localhost:3003/oapi',
+  CLOSE_API: 'https://mcs-platform-backend.herokuapp.com/api',
+  AUTH_API: 'https://mcs-platform-backend.herokuapp.com/oapi',
 }
 
 
